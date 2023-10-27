0aa55df3a466c8efaf85f135f1a406a7fb23ec3c
Andrei <ierdna100@yahoo.com>
2023-10-26 22:04:05 -0400

    Initial commit

---
+++ ReloadCustomScenarios/Main.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace ReloadCustomScenarios
+{
+    public class Main : VTOLMOD
+    {
+        public override void ModLoaded()
+        {
+            VTOLAPI.SceneLoaded += SceneLoaded;
+            base.ModLoaded();
+        }
+
+        private void SceneLoaded(VTOLScenes scene)
+        {
+            switch (scene)
+            {
+                case VTOLScenes.VTEditMenu:
+                    Debug.Log("[ReloadCustomScenariosMod] Scene \"VTEditMenu\" loaded! Reloading scenarios!");
+                    VTResources.LoadCustomScenarios();
+                    Debug.Log("[ReloadCustomScenariosMod] Reloaded scenarios");
+                    break;
+            }
+        }
+    }
+}
